ac4fb3b75f40efba3bf7b08269c426b7502e3709
shalu scaria <shalu.scaria3@gmail.com>
2018-08-14 00:41:48 -0700

    screenshot added

---
+++ homelib/homelib/Utilities/APIKey.swift
@@ -0,0 +1,11 @@
+//
+//  APIKey.swift
+//  homelib
+//
+//  Created by Shalu Scaria on 2018-08-13.
+//  Copyright © 2018 Shalu Scaria. All rights reserved.
+//
+
+import Foundation
+
+let API_KEY = "DPHQfnk3bP4uH9QEk3neS63IuGC2yiDz1PRc2FsE"
